c4bc175ced5bcbfd73c7d24a2cec72f609fafe28
Asif Mahamud <asif.cse04@gmail.com>
2023-09-13 12:28:54 +0600

    change in validation

--- matelso.viewmodels/RequestModel/ContactReqestModel.cs
+++ matelso.viewmodels/RequestModel/ContactReqestModel.cs
@@ -21,6 +21,8 @@ namespace matelso.viewmodels.RequestModel
         public DateTime? Birthdate { get; set; }
         
         public string? NotifyHasBirthdaySoon { get; set; }
+
+        [RegularExpression(@"^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$")]
         public string Email { get; set; }
         public string? Phonenumber { get; set; }
     }
